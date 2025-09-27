f137aa4a31045338a6d2039a30a8db50d30db6b7
RawatAman02 <amansinghrana094@gmail.com>
2025-09-27 15:27:12 +0530

    lab4.cpp

---
+++ C++ git/lab4.cpp
@@ -0,0 +1,10 @@
+ // To subtract two integers by user input
+ #include <iostream>
+using namespace std;
+int main(){
+  int num1,num2;
+  cout<<"Enter the two numbers:\n";
+  cin>>num1>>num2;
+   cout<<"num1-num2:"<<num1-num2<<endl;
+    return 0;
+}
