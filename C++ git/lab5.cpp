b6d140b97abe71a5832122a3d15ece9e64812449
RawatAman02 <amansinghrana094@gmail.com>
2025-09-27 15:30:01 +0530

    lab5.cpp

---
+++ C++ git/lab5.cpp
@@ -0,0 +1,9 @@
+// To multiply two floating numbers
+ #include <iostream>
+using namespace std;
+int main(){
+  float num1=6.8;
+  float num2=9.3;
+  cout<<"num1*num2:"<<num1*num2<<endl;
+    return 0;
+}
