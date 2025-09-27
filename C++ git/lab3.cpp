f9999443c84ea37e74c404d416493f34c6bc0011
RawatAman02 <amansinghrana094@gmail.com>
2025-09-27 15:18:40 +0530

    lab3.cpp

---
+++ C++ git/lab3.cpp
@@ -0,0 +1,10 @@
+ // adding to integers
+ #include <iostream>
+using namespace std;
+int main(){
+  int num1,num2;
+  num1 = 4;
+  num2 = 9;
+  cout<<"num1+num2:"<<num1+num2<<endl;
+    return 0;
+}
