6ca65ba2117d46ac76f9a0b3faf027bb893bd5f0
RawatAman02 <amansinghrana094@gmail.com>
2025-09-27 15:33:31 +0530

    lab6.cpp

---
+++ C++ git/lab6.cpp
@@ -0,0 +1,9 @@
+//To divide two integers and display quotient and remainder
+ #include <iostream>
+using namespace std;
+int main(){
+int num1=100,num2=5;
+ cout<<"Quotient :"<<num1/num2<<endl;
+ cout<<"Reaminder : "<<num1%num2<<endl; 
+    return 0;
+}
