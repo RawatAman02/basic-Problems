f953ca71a9f550d2dba825da4b73f2c0211b0a3c
RawatAman02 <amansinghrana094@gmail.com>
2025-09-27 15:48:11 +0530

    lab9.cpp

---
+++ C++ git/lab9.cpp
@@ -0,0 +1,10 @@
+//to find the size of float,int,char and doule datatypes
+ #include <iostream>
+using namespace std;
+int main(){
+ cout<<"Size of : "<<sizeof(float)<<"bytes\n";
+ cout<<"Size of : "<<sizeof(int)<<"bytes\n";
+ cout<<"Size of : "<<sizeof(char)<<"bytes\n";
+ cout<<"Size of : "<<sizeof(double)<<"bytes\n";
+ return 0;
+}
