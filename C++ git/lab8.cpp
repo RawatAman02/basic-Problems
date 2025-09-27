9a3346dc03a62c8f598f4298deece1b0ff400bd1
RawatAman02 <amansinghrana094@gmail.com>
2025-09-27 15:43:07 +0530

    lab8.cpp

---
+++ C++ git/lab8.cpp
@@ -0,0 +1,11 @@
+//To swapping two variable by without tempoary variable
+ #include <iostream>
+using namespace std;
+int main(){
+ int a=1,b=56;
+ a=a+b;
+ b= a-b;
+ a=a-b;
+ cout<<"a="<<a<<endl<<"b="<<b; 
+    return 0;
+}
