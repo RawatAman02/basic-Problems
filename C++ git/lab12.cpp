4d94ab20f6d3c4a7a43f71e5de3386d42a033dfb
Aman Rana <amansinghrana094@gmail.com>
2025-10-01 14:04:37 +0530

    lab12.cpp

---
+++ C++ git/lab12.cpp
@@ -0,0 +1,9 @@
+// program with logical error (area of rectangle)
+#include <iostream>
+using namespace std;
+int main(){
+    int l=5,b=9;
+    cout<<"area of rectangle:"<<l+b<<endl; // logical error 
+cout<<"area of rectangle : "<<l*b<<endl; // excepted answer
+    
+}
